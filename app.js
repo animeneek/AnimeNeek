0a0ff8e25e1e4a249801e3e52ae8f059ee400721
animeneek <imemyanime@gmail.com>
2025-04-13 10:40:14 +0400

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("AniNeek is live!");
-
-    const themeToggle = document.getElementById("themeToggle");
-
-    themeToggle.addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
-    });
-});
